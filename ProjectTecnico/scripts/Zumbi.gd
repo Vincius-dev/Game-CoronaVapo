2fdd79c5b5bd53fb96276d2d8c55024248a2f7f5
Vincius-dev <vinicius.fariascontato@gmail.com>
2020-11-04 16:03:00 -0300

    att

--- ProjectTecnico/scripts/Zumbi.gd
+++ ProjectTecnico/scripts/Zumbi.gd
@@ -7,23 +7,12 @@ var vida = 6
 
 
 func _ready():
-	$Sprite.play("Walk")
-	inicial = $".".position.x
-	final = inicial +100
+	
+	pass
 
 func _process(delta):
 	
-	if(inicial <= final and flip):
-		$".".position.x += velocidade
-		$Sprite.flip_h = false
-		if ($".".position.x >= final and flip):
-			flip = false
-			
-	if($".".position.x >= inicial and !flip):
-		$".".position.x -= velocidade
-		$Sprite.flip_h = true
-		if($".".position.x <= inicial):
-			flip = true
+	pass
 
 func DamageGlock():
 	vida -= 1
